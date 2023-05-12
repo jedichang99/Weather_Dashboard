aa8d2c009e5c4ddd53d583d0f1e6034b1e282823
Jedidiah Chang <jedichang99@gmail.com>
2023-05-11 22:12:16 -0500

    added pre-saved sities

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-The purpose of this project was to display the power of Java Script. Java Script is what makes this JS quiz work. it can test your knologe of JS and JS elements. 
+The purpose of this project was to display the power of Java Script and API's. I used a weather API that displays the wether acording to the city that you input. 
 
 ## Table of Contents
 
@@ -20,13 +20,13 @@ N/A
 
 ## Usage
 
-Now you can go about the web page and test your knologe of JS. Try to beet your own highscore. You can go the website with this URL. 
+Now you can go about the web page and test your knologe of JS. Try to find the weather for the city you live in. You can go the website with this URL. 
 https://jedichang99.github.io/Code-Quiz/
 
-![The Whole Page before you start the quiz](assets/photos/wholePage.png)
-![The Whole Page after you start the quiz](assets/photos/afterStart.png)
-![The high score button](assets/photos/highScore.png)
-![This is the timmer](assets/photos/timer.png)
+![The Whole Page](assets/photos/whole_page.png)
+![Pre-saved citys](assets/photos/pre-set_citys.png)
+![Enter a city in this box](assets/photos/Enter_a_city.png)
+![Forcast Section](assets/photos/forcast.png)
 
 ## Credits
 
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -137,3 +137,82 @@ $("#search-form").on("submit", function (event) {
     $("#search-history").append(listItem);
   });
 });
+
+const cityButtons = document.querySelectorAll(".city-btn");
+
+for (let i = 0; i < cityButtons.length; i++) {
+  cityButtons[i].addEventListener("click", function (event) {
+    var cities = event.target.dataset.city;
+    $.ajax({
+      //const city = $("#city-input").val();
+      url: `${BASE_URL}weather?q=${cities}&units=metric&appid=${API_KEY}`,
+      method: "GET",
+      success: function (response) {
+        // Extract relevant data from response
+        const cityName = response.name;
+        const date = new Date(response.dt * 1000);
+        const icon = response.weather[0].icon;
+        const temp = response.main.temp;
+        const humidity = response.main.humidity;
+        const windSpeed = response.wind.speed;
+
+        // Create current weather HTML
+        const currentWeatherHTML = `
+        <h3>${cityName} (${date.toLocaleDateString()}) <img src="http://openweathermap.org/img/w/${icon}.png" alt="${
+          response.weather[0].description
+        }"></h3>
+        <p>Temperature: ${temp} °C</p>
+        <p>Humidity: ${humidity}%</p>
+        <p>Wind Speed: ${windSpeed} m/s</p>
+      `;
+
+        // Display current weather HTML
+        $("#current-weather").html(currentWeatherHTML);
+
+        // Save city to local storage
+        saveToLocalStorage(cityName, icon, temp, humidity, windSpeed);
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    });
+    $.ajax({
+      url: `${BASE_URL}forecast?q=${cities}&units=metric&cnt=5&appid=${API_KEY}`,
+      method: "GET",
+      success: function (response) {
+        // Extract relevant data from response
+        const forecast = response.list.map((day) => ({
+          date: new Date(day.dt * 1000),
+          icon: day.weather[0].icon,
+          temp: day.main.temp,
+          humidity: day.main.humidity,
+          windSpeed: day.wind.speed,
+        }));
+
+        // Render forecast HTML
+        const forecastHtml = forecast
+          .map(
+            (day) => `
+                <div class="forecast-day">
+                  <div class="forecast-date">${day.date.toLocaleDateString()}</div>
+                  <div class="forecast-icon"><img src="http://openweathermap.org/img/w/${
+                    day.icon
+                  }.png"></div>
+                  <div class="forecast-temp">${day.temp.toFixed(1)}°C</div>
+                  <div class="forecast-humidity">Humidity: ${
+                    day.humidity
+                  }%</div>
+                  <div class="forecast-wind">Wind: ${day.windSpeed} m/s</div>
+                </div>
+              `
+          )
+          .join("");
+
+        $("#forecast").html(forecastHtml);
+      },
+      error: function () {
+        alert("Failed to retrieve 5-day forecast data.");
+      },
+    });
+  });
+}
